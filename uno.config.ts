bedce17daa1905e6a583b32dbfe88f541d59203d
Prajeen Govardhanam <contact@prajeen.com>
2024-06-11 09:22:37 +0530

    :wrench: Configure unocss presets
    Signed-off-by: Prajeen Govardhanam <contact@prajeen.com>

--- uno.config.ts
+++ uno.config.ts
@@ -1,3 +1,21 @@
-import { defineConfig } from "unocss";
+import { defineConfig, presetAttributify, presetUno, presetWebFonts } from "unocss";
 
-export default defineConfig({});
+export default defineConfig({
+  presets: [
+    presetAttributify({}),
+    presetUno(),
+    presetWebFonts({
+      provider: "bunny",
+      fonts: {
+        sans: {
+          name: "Satoshi",
+          provider: "fontshare"
+        },
+        mono: ["FiraCode", "FiraMono"],
+        brand: {
+          name: "Overpass"
+        }
+      }
+    })
+  ]
+});
