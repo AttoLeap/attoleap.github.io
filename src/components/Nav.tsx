87af9b4ef3338f656bc9fd64fdd919ce57546d2a
Prajeen Govardhanam <prajeen.govardhanam@attoleap.com>
2024-06-15 21:09:25 +0530

    :construction: Add functions to hamburger menu
    Signed-off-by: Prajeen Govardhanam <prajeen.govardhanam@attoleap.com>

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,38 +2,63 @@ import { A, useLocation } from "@solidjs/router";
 import { createSignal } from "solid-js";
 import logo from "~/assets/dark-logo.svg";
 
-export default function Nav() {
+function NavLinks(props: { isColumn?: boolean }) {
   const location = useLocation();
   const active = (path: string) =>
     path === location.pathname ? "text-color-gray-100" : "text-color-gray-400";
 
+  return (
+    <ul flex={`~ ${props.isColumn ? "col" : "row"}`} gap-4>
+      <li class={`${active("/")}`}>
+        <A href="/">Home</A>
+      </li>
+      <li class={`${active("/about")}`}>
+        <A href="/about">About</A>
+      </li>
+    </ul>
+  );
+}
+
+export default function Nav() {
   const [menuOpen, setMenuOpen] = createSignal(false);
+  const toggleMenu = () => setMenuOpen(!menuOpen());
 
   return (
-    <nav flex="~ row" h="72px" w="full" place="items-center" justify="between" bg="black" p="4">
-      <div flex="~ flex-row" place="items-center" gap="4">
-        <A href="/">
-          <img w="32px" h="32px" src={logo} alt="Attoleap's white logo" />
-        </A>
-        <h1 font="brand 900" text="size-28px white" tracking-wide>
-          attoleap
-        </h1>
+    <>
+      <nav flex="~ row" h="72px" w="full" place="items-center" justify="between" bg="black" p="4">
+        <div flex="~ flex-row" place="items-center" gap="4">
+          <A href="/">
+            <img w="32px" h="32px" src={logo} alt="Attoleap's white logo" />
+          </A>
+          <h1 font="brand 900" text="size-28px white" tracking-wide>
+            attoleap
+          </h1>
+        </div>
+        <div place="items-center" gap="4" font="sans 600" text="size-sm" uppercase hidden md="flex">
+          <NavLinks />
+        </div>
+        <span
+          md="hidden"
+          class={menuOpen() ? "i-lucide-x" : "i-lucide-menu"}
+          text="color-white size-3xl"
+          transition="~ duration-200 ease-in-out"
+          onClick={toggleMenu}
+        />
+      </nav>
+      <div
+        class={menuOpen() ? "flex md:hidden" : "hidden"}
+        absolute
+        bg="black"
+        font="600"
+        text="size-3xl"
+        p="16"
+        w="full"
+        h="[calc(100%_-_72px)]"
+        transition="~ duration-200 ease-in-out"
+        onClick={toggleMenu}
+      >
+        <NavLinks isColumn />
       </div>
-      <ul place="items-center" gap="4" font="sans 600" text="size-sm" uppercase hidden md="flex">
-        <li class={`${active("/")}`}>
-          <A href="/">Home</A>
-        </li>
-        <li class={`${active("/about")}`}>
-          <A href="/about">About</A>
-        </li>
-      </ul>
-      <span
-        md="hidden"
-        class={menuOpen() ? "i-lucide-x" : "i-lucide-menu"}
-        text="color-white size-3xl"
-        transition="ease-in-out"
-        onClick={() => setMenuOpen(!menuOpen())}
-      />
-    </nav>
+    </>
   );
 }
