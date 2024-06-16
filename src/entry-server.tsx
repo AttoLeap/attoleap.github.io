a7ec3430f05977b68f702c75aedb1f443f9ca940
Prajeen Govardhanam <prajeen.govardhanam@attoleap.com>
2024-06-17 00:12:35 +0530

    :art: Add meta description tag for all pages
    Signed-off-by: Prajeen Govardhanam <prajeen.govardhanam@attoleap.com>

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -8,6 +8,10 @@ export default createHandler(() => (
         <head>
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta
+            name="description"
+            content="AttoLeap is a tech startup dedicated to providing innovative solutions for valuable customer problems. We as an organization are passionate about giving back to opensource community and make it thrive as much as we can."
+          />
           <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
           <link rel="icon" type="image/png" href="/favicon.png" />
           {assets}
